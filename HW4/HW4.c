158b2c0ac018bd9482af3047b1be6dc2f0bf52f7
Charlizeguillen <charlizeguillen-mejia2026@u.northwestern.edu>
2026-04-11 14:25:42 -0500

    updated HW4 with single letter (pt. 2)

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -12,6 +12,25 @@
 #define SCL_PIN 5
 
 
+//function to draw letter at (x,y) using the ASCII lookup table (pt. 2)
+void drawChar(unsigned char x, unsigned char y, char c) {
+    int letter = c - 0x20;   // convert ASCII character to font index
+
+    if (letter < 0 || letter > 95) {
+        return;   // ignore unsupported characters
+    }
+
+    for (int col = 0; col < 5; col++) { // each character is 5 columns wide
+        char column_data = ASCII[letter][col];  // get the column data for character
+
+        for (int row = 0; row < 8; row++) { // each column is 8 bits tall
+            int bit = (column_data >> row) & 0x01; //for pixel on/off
+            ssd1306_drawPixel(x + col, y + row, bit);
+        }
+    }
+}
+
+
 int main() {
     stdio_init_all();
 
@@ -32,16 +51,27 @@ int main() {
     int state = 0;
 
     while (1) {
+        //blink pico led 
         state = !state;
-
-        //blink pico led
         gpio_put(LED_PIN, state);
 
-        //blink oled pixel
+        //-----blink oled pixel (pt. 1)-----
+        //ssd1306_clear();
+        //ssd1306_drawPixel(10, 10, state);
+        //ssd1306_update();
+
+        //-----draw character (pt. 2)-----
         ssd1306_clear();
-        ssd1306_drawPixel(10, 10, state);
+        drawChar(10, 10, 'A');
         ssd1306_update();
 
-        sleep_ms(500); // ~1 Hz
+        sleep_ms(500);
+
+        gpio_put(25, 0);
+
+        ssd1306_clear();
+        ssd1306_update();
+
+        sleep_ms(500);
     }
 }
