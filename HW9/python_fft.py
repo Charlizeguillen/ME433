f589e95f6150b6a1afbc41a496fcd23d5216a227
Charlizeguillen <charlizeguillen-mejia2026@u.northwestern.edu>
2026-04-29 20:45:02 -0500

    updated python_fft.py

--- HW9/python_fft.py
+++ HW9/python_fft.py
@@ -7,21 +7,102 @@ import numpy as np
 #s = 4.0 * np.sin(2 * np.pi * 100 * t) + 0.25 * np.sin(2 * np.pi * 1000 * t) + 25
 
 #gatting data from file
-filename = 'sigD.csv' #change file name for other signals
+filename = 'sigC.csv' #change file name for other signals
 
 #for moving average filter
 #X= 10 
 
 #for IIR filter
-A = 0.95 #change for different cutoff frequencies, smaller is lower cutoff frequency
-B = 1 - A
+A = 0.9
+B = 0.1
 
-data = np.genfromtxt('sigD.csv', delimiter=',')  #change file name for other signals
+data = np.genfromtxt(filename, delimiter=',')  #change file name for other signals
 t= data[:,0]
 s= data[:,1]
-Fs= len(t)/t[-1] # sample rate    
+Fs= len(t)/t[-1] # sample rate 
+print('Sample rate: ' + str(Fs) + ' Hz')   
 
-filtered = []
+#filtered = []
+h = np.array([0.000550283536446716,
+    0.000669295562481363,
+    0.000725049084885430,
+    0.000695746364512743,
+    0.000553181037731246,
+    0.000271460844301492,
+    -0.000160118396348936,
+    -0.000723290750324895,
+    -0.001360327841953853,
+    -0.001971027653998972,
+    -0.002420289983048358,
+    -0.002557336588935020,
+    -0.002244973835174325,
+    -0.001394546873944762,
+    0.000000000000000002,
+    0.001836701746656515,
+    0.003896222513431482,
+    0.005854090584586322,
+    0.007316683424242810,
+    0.007878835452694664,
+    0.007196428844128102,
+    0.005063438453233345,
+    0.001480445790163525,
+    -0.003298766762015542,
+    -0.008753984670937607,
+    -0.014125947667245113,
+    -0.018485698539391957,
+    -0.020840478812437453,
+    -0.020264235026079800,
+    -0.016035646196701506,
+    -0.007763768307303201,
+    0.004518449922367797,
+    0.020309223323408971,
+    0.038651901165652057,
+    0.058210123107134283,
+    0.077397576433913304,
+    0.094547499875283325,
+    0.108100711754370901,
+    0.116787530891422020,
+    0.119779116385585874,
+    0.116787530891422034,
+    0.108100711754370915,
+    0.094547499875283325,
+    0.077397576433913304,
+    0.058210123107134283,
+    0.038651901165652057,
+    0.020309223323408974,
+    0.004518449922367797,
+    -0.007763768307303203,
+    -0.016035646196701506,
+    -0.020264235026079797,
+    -0.020840478812437453,
+    -0.018485698539391950,
+    -0.014125947667245110,
+    -0.008753984670937609,
+    -0.003298766762015542,
+    0.001480445790163525,
+    0.005063438453233348,
+    0.007196428844128102,
+    0.007878835452694667,
+    0.007316683424242815,
+    0.005854090584586324,
+    0.003896222513431484,
+    0.001836701746656515,
+    0.000000000000000002,
+    -0.001394546873944764,
+    -0.002244973835174325,
+    -0.002557336588935024,
+    -0.002420289983048359,
+    -0.001971027653998974,
+    -0.001360327841953853,
+    -0.000723290750324895,
+    -0.000160118396348936,
+    0.000271460844301492,
+    0.000553181037731246,
+    0.000695746364512743,
+    0.000725049084885430,
+    0.000669295562481363,
+    0.000550283536446716])   # paste from website
+filtered = np.convolve(s, h, mode='same')
 
 #----for average filter
 #for i in range(len(s)):
@@ -32,11 +113,13 @@ filtered = []
 #    filtered.append(avg)
 
 #----for IIR filter
-for i in range(1, len(s)):
-    new_value = A * filtered[i-1] + B * s[i]
-    filtered.append(new_value)
+#filtered.append(s[0]) #initial value 
+#for i in range(1, len(s)):
+#    new_value = A * filtered[i-1] + B * s[i]
+#    filtered.append(new_value)
 
-filtered = np.array(filtered)
+
+#filtered = np.array(filtered)
 
 #For original FFT
 #Fs = 10000 # sample rate
@@ -60,11 +143,11 @@ Yf = Yf[range(int(n/2))]
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
 
-ax1.plot(t,s,'b', label='Unfiltered')
+ax1.plot(t,s,'k', label='Unfiltered')
 ax1.plot(t,filtered,'r', label='Filtered')
 ax1.set_xlabel('Time [s]')
 ax1.set_ylabel('Amplitude')
-ax1.set_title(filename + 'IIR Filter, A=' + str(A), ', B=' + str(B))
+ax1.set_title(f'{filename} FIR (Hamming), taps={len(h)}, fc=150 Hz, bw=100 Hz')
 ax1.legend()
 
 # plotting the fft
